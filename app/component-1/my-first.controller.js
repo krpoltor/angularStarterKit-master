53c9a0799a6f19a15b3c91ab6aa629e86f97449e
krpoltor <krzysztof.poltorak@capgemini.com>
2016-09-05 11:38:55 +0200

    finished 1.1

--- app/component-1/my-first.controller.js
+++ app/component-1/my-first.controller.js
@@ -1,8 +1,23 @@
-angular.module('app.component1').controller('MyFirstController', function($scope, $http, $modal, books){
-   'use strict';
+angular.module('app.component1')
 
-
-}).controller('MyModalController', function($scope, $modalInstance, selectedBook){
+.controller('MyFirstController', function($scope, $http, $modal, books) {
     'use strict';
 
-})
+    $scope.books = null;
+
+    $http.get('/component-1/books.json')
+        .success(function(data) {
+            $scope.books = data;
+        })
+        .error(function(data, status, error, config) {
+            $scope.books = [{
+                title: 'Error',
+                author: 'Could not load json data'
+            }];
+        });
+
+
+}).controller('MyModalController', function($scope, $modalInstance, selectedBook) {
+    'use strict';
+
+});
